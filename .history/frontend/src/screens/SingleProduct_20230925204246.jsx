fba7cbe7b2a643391d5b22b7c6760e9eed286dc0
Safa Rahaf <safarahafkhan@gmail.com>
2023-09-25 20:43:46 +0600

    single page created

--- .history/frontend/src/screens/SingleProduct_20230925204246.jsx
+++ .history/frontend/src/screens/SingleProduct_20230925204246.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import Rating from '../components/HomeComponnets/rating';
 import Message from '../components/LoadingError/Error';
